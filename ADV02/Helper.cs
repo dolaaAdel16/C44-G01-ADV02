849b1e917a9684977eefda2d6a9cfb2d66f36351
Ahmed <aa3452000@gmail.com>
2025-08-10 21:23:57 +0300

    Helper

---
+++ ADV02/Helper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADV02
+{
+    internal class Helper
+    {
+        public static int SumList(List<int> list)
+        {
+            int Sum = 0;
+            if (list is not null)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Sum += list[i]; // Sum = Sum +list[i]
+                }
+
+
+            }
+            return Sum;
+
+        }
+    }
+}
